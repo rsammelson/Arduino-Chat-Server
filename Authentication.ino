a06a6d06c451e93bfbf67f7afff2af2d97fe4cb0
Robby Sammelson <rsamm3@gmail.com>
2017-02-27 21:06:26 -0500

    Added authentication
    started authntication system, to force people to use specific names

---
+++ Authentication.ino
@@ -0,0 +1,14 @@
+bool is_authentified(){
+  Serial.println("Enter is_authentified");
+  if (server.hasHeader("Cookie")){   
+    Serial.print("Found cookie: ");
+    String cookie = server.header("Cookie");
+    Serial.println(cookie);
+    if (cookie.indexOf("ESPSESSIONID=1") != -1) {
+      Serial.println("Authentification Successful");
+      return true;
+    }
+  }
+  Serial.println("Authentification Failed");
+  return false;  
+}
